974f7eea260695c71ce8a8607d610522ba9c6934
Mahmood - Zer0xFF <5013823+Zer0xFF@users.noreply.github.com>
2020-06-05 00:04:33 +0100

    test fused multadd against non fused mult+add

--- tests/DoubleFusedMultiAddTest.cpp
+++ tests/DoubleFusedMultiAddTest.cpp
@@ -37,19 +37,22 @@ void CDoubleFusedMultiAddTest::Compile(Jitter::CJitter& jitter)
 		jitter.MD_PushRel(offsetof(CONTEXT, number3));
 		jitter.MD_PushRel(offsetof(CONTEXT, number2));
 		jitter.MD_PushRel(offsetof(CONTEXT, number1));
-		jitter.MD_MulSub();
+		jitter.MD_MulS();
+		jitter.MD_AddS();
 		jitter.MD_PullRel(offsetof(CONTEXT, res4));
 
 		jitter.MD_PushRel(offsetof(CONTEXT, number4));
 		jitter.MD_PushRel(offsetof(CONTEXT, number2));
 		jitter.MD_PushRel(offsetof(CONTEXT, number2));
-		jitter.MD_MulSub();
+		jitter.MD_MulS();
+		jitter.MD_AddS();
 		jitter.MD_PullRel(offsetof(CONTEXT, res5));
 
 		jitter.MD_PushRel(offsetof(CONTEXT, number3));
 		jitter.MD_PushRel(offsetof(CONTEXT, number4));
 		jitter.MD_PushRel(offsetof(CONTEXT, number2));
-		jitter.MD_MulSub();
+		jitter.MD_MulS();
+		jitter.MD_AddS();
 		jitter.MD_PullRel(offsetof(CONTEXT, res6));
 	}
 	jitter.End();
@@ -60,7 +63,6 @@ void CDoubleFusedMultiAddTest::Compile(Jitter::CJitter& jitter)
 void CDoubleFusedMultiAddTest::Run()
 {
 	memset(&m_context, 0, sizeof(CONTEXT));
-    // placeholder test/broken
 	m_context.number1[1] = 1 ;
 	m_context.number2[1] = 2 ;
 	m_context.number3[1] = 4 ;
@@ -70,11 +72,10 @@ void CDoubleFusedMultiAddTest::Run()
 	m_context.number3[2] = 4 ;
 	m_context.number4[2] = 16;
 	m_function(&m_context);
-	TEST_VERIFY(m_context.res1[0] == 1.0);
-	// TEST_VERIFY(m_context.res2 == 20.0f);
-	// TEST_VERIFY(m_context.res3 == 28.0f);
-
-	// TEST_VERIFY(m_context.res4 == 6.0f);
-	// TEST_VERIFY(m_context.res5 == -12.0f);
-	// TEST_VERIFY(m_context.res6 == 36.0f);
+	for(int i = 0; i < 4; ++i)
+	{
+		TEST_VERIFY(m_context.res1[i] == m_context.res4[i]);
+		TEST_VERIFY(m_context.res2[i] == m_context.res5[i]);
+		TEST_VERIFY(m_context.res3[i] == m_context.res6[i]);
+	}
 }
